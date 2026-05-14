1455126358fe6731bf8f30d71046ac10afa75131
DemianTarasov <demian17.03.2009@mail.ru>
2026-05-14 11:04:36 +0400

    auth
    реализация первой авторизации

--- back_repet_ai/internal/postgres/db.go
+++ back_repet_ai/internal/postgres/db.go
@@ -1,28 +1,42 @@
-package posgres
+package postgres
 
 import (
 	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	_ "github.com/krisikas/CU_repet_ai/back_repet_ai/internal/model"
+	"github.com/krisikas/CU_repet_ai/back_repet_ai/internal/model"
 )
 
-
-
 type Postgres struct {
-	db *gorm.DB
+	DB *gorm.DB
 }
 
 func NewDB(host, user, password, dbname string) (*Postgres, error) {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable", 
-		host, user, password, dbname)
-	
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	fmt.Println(dbname)
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:5432/%s?sslmode=disable", 
+		user, password, host, dbname)
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN: dsn,
+	}), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
 
-	return &Postgres{
-		db: db,
-	}, nil
+	db.AutoMigrate(&model.User{})
+
+	return &Postgres{DB: db}, nil
+}
+
+func (p *Postgres) CreateUser(user *model.User) error {
+	return p.DB.Create(user).Error
+}
+
+func (p *Postgres) GetUserByLogin(login string) (*model.User, error) {
+	var user model.User
+	err := p.DB.Where("login = ?", login).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
